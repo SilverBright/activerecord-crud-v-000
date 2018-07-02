5cbb3d611ad1379ac40fd7c23ce653ee617976c0
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-02 18:36:04 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = "Movie_#{i}"
+    m.release_date = i+2000
   end
 end
 
